68a5f1725717f49eeaec59158e8c24ead0716646
Tomáš Valenta <semanticer@gmail.com>
2016-04-23 01:16:42 +0200

    support for embeded youtube videos

---
+++ app/src/main/java/cz/damematiku/damematiku/data/DeveloperKey.java
@@ -0,0 +1,8 @@
+package cz.damematiku.damematiku.data;
+
+/**
+ * Created by semanticer on 23. 4. 2016.
+ */
+public class DeveloperKey {
+    public static final String DEVELOPER_KEY = "AIzaSyBMJNacJ08RLyQn1otZf7gLtUitNvp0VAQ";
+}
--- app/src/main/java/cz/damematiku/damematiku/presentation/video/VideoActivity.java
+++ app/src/main/java/cz/damematiku/damematiku/presentation/video/VideoActivity.java
@@ -9,34 +9,55 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import cz.damematiku.damematiku.R;
+import cz.damematiku.damematiku.data.DeveloperKey;
 import cz.damematiku.damematiku.data.model.Video;
 
-public class VideoActivity extends AppCompatActivity implements VideoView {
+public class VideoActivity extends YouTubeFailureRecoveryActivity implements VideoView {
 
     private static final String ARG_VIDEO = "ARG_VIDEO";
 
-    @Bind(R.id.toolbar)
-    Toolbar toolbar;
+//    @Bind(R.id.toolbar)
+//    Toolbar toolbar;
+
+    @Bind(R.id.youtube_view)
+    YouTubePlayerView youTubePlayerView;
 
     VideoPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_video);
+        setContentView(R.layout.activity_video_test);
         ButterKnife.bind(this);
-        setSupportActionBar(toolbar);
+        youTubePlayerView.initialize(DeveloperKey.DEVELOPER_KEY, this);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-        Video video = getIntent().getParcelableExtra(ARG_VIDEO);
+//        setSupportActionBar(toolbar);
 
-        presenter = new VideoPresenter();
-        presenter.setData(video);
-        presenter.setView(this);
-        presenter.start();
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+//        Video video = getIntent().getParcelableExtra(ARG_VIDEO);
+//
+//        presenter = new VideoPresenter();
+//        presenter.setData(video);
+//        presenter.setView(this);
+//        presenter.start();
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player, boolean wasRestored) {
+        if (!wasRestored) {
+            player.cueVideo("wKJ9KzGQq0w");
+        }
+    }
+
+    @Override
+    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return (YouTubePlayerView) findViewById(R.id.youtube_view);
     }
 
     public static Intent create(Context context, Video video) {
