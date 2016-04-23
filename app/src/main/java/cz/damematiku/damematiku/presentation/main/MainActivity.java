63ea14c0eaba0cb4603854f5bedfc6292dbcc604
Tomáš Valenta <semanticer@gmail.com>
2016-04-23 12:59:49 +0200

    searchbar styling

--- app/src/main/java/cz/damematiku/damematiku/presentation/main/MainActivity.java
+++ app/src/main/java/cz/damematiku/damematiku/presentation/main/MainActivity.java
@@ -35,7 +35,6 @@ import cz.damematiku.damematiku.presentation.chapter.ChapterActivity;
 
 public class MainActivity extends AppCompatActivity implements MainView, SectionAdapter.ChapterClickListener {
 
-
     @Bind(R.id.toolbar)
     Toolbar toolbar;
 
@@ -87,7 +86,7 @@ public class MainActivity extends AppCompatActivity implements MainView, Section
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
